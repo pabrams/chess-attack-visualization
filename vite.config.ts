c356122db1284742b8d479213f366d5c6207a2db
Paul Abrams <john.paul.abrams@gmail.com>
2025-10-27 12:31:32 -0400

    hack: neverending config just to use modified chess.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+    resolve: {
+    alias: {
+      'chess.js': path.resolve(__dirname, 'local-mods/chess.js/dist/esm/chess.js'),
+    },
+  },
   server: {
     host: '0.0.0.0',
     port: 5173,
