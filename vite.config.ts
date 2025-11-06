d69c5dd494475ed1f3377e08c0da60fdc8a47ff0
Paul Abrams <john.paul.abrams@gmail.com>
2025-11-05 19:46:34 -0500

    simplify: hardcode base path to /monkey-drill/ in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const isProduction = process.env.NODE_ENV === 'production';
-const ghPagesMode = process.env.GH_PAGES === 'true';
-
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -16,5 +13,5 @@ export default defineConfig({
       usePolling: true,
     },
   },
-  base: ghPagesMode ? '/monkey-drill/' : '/',
+  base: '/monkey-drill/',
 });
