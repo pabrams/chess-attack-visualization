3a1489b13377483e78dfc8f5874c45d1a0d042a6
Paul Abrams <john.paul.abrams@gmail.com>
2025-12-12 16:13:04 -0500

    refactor: useArrows

--- src/hooks/useArrows.ts
+++ src/hooks/useArrows.ts
@@ -18,11 +18,9 @@ export const useArrows = ({ chessGame }: UseArrowsProps) => {
   const whiteArrowColor = getCSSVar('--chess-white-arrow');
   const blackArrowColor = getCSSVar('--chess-black-arrow');
   const [arrows, setArrows] = useState<Arrow[]>([]);
-  const [marks, setMarks] = useState<Mark[]>([]);
 
   const clearArrows = useCallback(() => {
     setArrows([]);
-    setMarks([]);
   }, []);
 
   const showAttackersForSquare = useCallback((square: Square) => {
@@ -71,31 +69,24 @@ export const useArrows = ({ chessGame }: UseArrowsProps) => {
     }
 
     const arrowColor = checkmatingColor === 'w' ? whiteArrowColor : blackArrowColor;
-    const defendingColor = checksColor;
-    const defendingArrowColor = defendingColor === 'w' ? whiteArrowColor : blackArrowColor;
-
     const newArrows: Arrow[] = [];
-    const newMarks: Mark[] = [];
 
     const kingAttackers = chessGame.getAttackers(kingSquare, checkmatingColor);
     newArrows.push(...createArrowsFromAttackers(kingAttackers, kingSquare, arrowColor));
 
     const aroundSquares = getAdjacentSquares(kingSquare);
-    const { arrows: escapeArrows, marks: escapeMarks } = createArrowsForSquaresAroundKing(
+    const { arrows: escapeArrows } = createArrowsForSquaresAroundKing(
       aroundSquares,
       chessGame.getAttackers,
       checkmatingColor,
       chessGame.getPieceAt,
       arrowColor,
-      defendingColor
     );
 
     newArrows.push(...escapeArrows);
-    newMarks.push(...escapeMarks.map(mark => ({ ...mark, color: defendingArrowColor })));
 
     const arrowEndSquares = new Set<Square>();
 
-    // From king attacker arrows
     newArrows.slice(0, kingAttackers.length).forEach(arrow => {
       arrowEndSquares.add(arrow.endSquare);
     });
@@ -104,24 +95,7 @@ export const useArrows = ({ chessGame }: UseArrowsProps) => {
       arrowEndSquares.add(arrow.endSquare);
     });
 
-    arrowEndSquares.forEach(square => {
-      const markExists = newMarks.some(mark => mark.square === square);
-      if (!markExists) {
-        newMarks.push({ square, color: arrowColor });
-      }
-    });
-
-    // Sort by arrow length so longer arrows are drawn last (on top)
-    newArrows.sort((a, b) => {
-      const aLen = Math.pow(a.endSquare.charCodeAt(0) - a.startSquare.charCodeAt(0), 2) +
-                   Math.pow(parseInt(a.endSquare[1]) - parseInt(a.startSquare[1]), 2);
-      const bLen = Math.pow(b.endSquare.charCodeAt(0) - b.startSquare.charCodeAt(0), 2) +
-                   Math.pow(parseInt(b.endSquare[1]) - parseInt(b.startSquare[1]), 2);
-      return aLen - bLen;
-    });
-
     setArrows(newArrows);
-    setMarks(newMarks);
   }, [chessGame, whiteArrowColor, blackArrowColor, clearArrows]);
 
   // When board position changes, clear and re-evaluate for checkmate
@@ -132,7 +106,6 @@ export const useArrows = ({ chessGame }: UseArrowsProps) => {
 
   return {
     arrows,
-    marks,
     clearArrows,
     showCheckmaters,
     handleSquareRightClick,
